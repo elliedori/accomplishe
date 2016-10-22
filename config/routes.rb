4d0614904382619b5d5d738f3805965c8e8121ed
kimallen <kimmarieallen@gmail.com>
2016-10-22 15:48:56 -0700

    Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'model/Tag'
 
-  resources :users
-  resources :posts
+  resources :users do
+	  resources :posts
+  end
+  
   resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
