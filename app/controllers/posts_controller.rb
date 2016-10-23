74cbc57b41540c98c47e83886782fa5693d93b92
kimallen <kimmarieallen@gmail.com>
2016-10-22 20:36:31 -0700

    Add routes for posts by category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,32 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all
+		@power_posts = @posts.where(tag_id: 1)
+		@growth_posts = @posts.where(tag_id: 2)
+		@facts_posts = @posts.where(tag_id: 3)
+		@endorsements_posts = @posts.where(tag_id: 4)
+	
+	end
 
+	def power
+		posts = Post.all
+		@power_posts = posts.where(tag_id: 1)
+	end
+
+	def growth
+		posts = Post.all
+		@growth_posts = posts.where(tag_id: 2)
+	end
+
+	def facts
+		posts = Post.all
+		@facts_posts = posts.where(tag_id: 3)
+	end
+
+	def kudos
+		posts = Post.all
+		@kudos_posts = posts.where(tag_id: 4)
 	end
 
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   get 'posts/index'
-  root 'posts#index'
-
-  get 'model/Tag'
+  # root 'posts#index'
+  root 'home#index'
 
   resources :users do
 	  resources :posts
   end
 
   resources :tags
+
+  get 'posts/power', to: 'posts#power'
+  get 'posts/growth', to: 'posts#growth'
+  get 'posts/facts', to: 'posts#facts'
+  get 'posts/kudos', to: 'posts#kudos'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
