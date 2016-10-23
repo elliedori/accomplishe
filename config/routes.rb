f15bbf865b588bd6db6472b913233a08759baccd
Sean Lee <seanboramlee@gmail.com>
2016-10-22 17:02:31 -0700

    add def index to posts controller, add root to config/routes, and render hello world to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  root 'posts#index'
+
   get 'model/Tag'
 
   resources :users do
 	  resources :posts
   end
-  
+
   resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
